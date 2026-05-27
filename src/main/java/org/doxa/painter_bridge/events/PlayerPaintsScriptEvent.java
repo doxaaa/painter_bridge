de0c28d871bb8deaad0c3cb99e68ec1a98232b2b
doxaaa <zparadoxa@gmail.com>
2026-05-27 21:32:35 +1000

    update

--- src/main/java/org/doxa/painter_bridge/events/PlayerPaintsScriptEvent.java
+++ src/main/java/org/doxa/painter_bridge/events/PlayerPaintsScriptEvent.java
@@ -31,7 +31,7 @@ public class PlayerPaintsScriptEvent extends BukkitScriptEvent implements Listen
     //
     // @Player Always.
     //
-    // @Group painter_bridge
+    // @Group PainterBridge
     //
     // -->
 
